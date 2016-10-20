ef1111c59a3e015ad29250637f9683b303208800
HarryMcKinney <harrypmckinney@gmail.com>
2016-10-20 11:46:10 -0400

    remove heatmap

---
+++ js/script.js
@@ -0,0 +1,3 @@
+console.info("Welcome to the Good things about Racing Innovations :)");
+console.info("we have 1000's and 1000's of photos to post!");
+console.info("this site is 100% open source and hosted on Github - H");
